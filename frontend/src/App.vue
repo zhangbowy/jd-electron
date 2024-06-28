bfc2e0f5925616c06dd61f96e28b8036126c7269
zhangbo <1751170889@qq.com>
2024-06-28 16:01:28 +0800

    add

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,19 +7,43 @@
 <script>
 import { ipcApiRoute } from '@/api/main';
 import { ipc } from '@/utils/ipcRenderer';
+import {message} from "ant-design-vue";
 export default {
   name: 'App',
   setup() {
-    document.getElementById('loadingPage').remove()
+    // document.getElementById('loadingPage').remove()
   },
   created() {
-    this.loginWindow()
+    this.checkLogin()
   },
   methods: {
+    checkLogin() {
+      fetch('http://122.51.6.129:8001/admin/Zt/index', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        body: JSON.stringify({})
+      }).then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if(data.status === 411) {
+            this.loginWindow()
+            return message.error('该账号已被禁用, 请联系管理员', 6000)
+          }
+          if(data.status === 200) {
+            // if (data.admin.status === 0) {
+            //   alert('该账号已被禁用, 请联系管理员')
+            //   return
+            //   // return message.error('该账号已被禁用, 请联系管理员', 6000)
+            // }
+          } else {
+            this.loginWindow()
+          }
+        })
+    },
     loginWindow () {
       this.$router.push({ name: 'SpecialLoginWindow', params: {}});
-      ipc.invoke(ipcApiRoute.loginWindow, {width: 800, height: 600}).then(r => {      
-        // 
+      ipc.invoke(ipcApiRoute.loginWindow, {width: 400, height: 300}).then(r => {
+        //
       })
     },
   }
--- frontend/src/views/effect/login/Window.vue
+++ frontend/src/views/effect/login/Window.vue
@@ -1,37 +1,59 @@
 <template>
   <div id="effect-login-window">
     <div class="block-1">
-      <a-input></a-input>
-      <a-input></a-input>
+      <a-input style="margin-bottom: 10px" v-model:value="username" placeholder="请输入账号"></a-input>
+      <a-input style="margin-bottom: 10px"  v-model:value="password" placeholder="请输入密码"></a-input>
       <a v-if="!loading" @click="login">
         <a-button type="primary">
           登录
         </a-button>
       </a>
       <span v-else>{{ loginText }}</span>
-    </div>  
+    </div>
   </div>
 </template>
 <script>
 import { ipcApiRoute } from '@/api/main';
 import { ipc } from '@/utils/ipcRenderer';
-  
+import { message } from 'ant-design-vue';
+
 export default {
   data() {
     return {
-      loading: false, 
-      loginText: '正在登陆......'
+      loading: false,
+      loginText: '正在登陆......',
+      username: "",
+      password: "",
     };
   },
   methods: {
+
     login() {
       this.loading = true;
-      setTimeout(() => {
-        this.$router.push({ name: 'OsWindowViewIndex', params: {}});
-        ipc.invoke(ipcApiRoute.restoreWindow, {width: 980, height: 650}).then(r => {      
-          // 
+      fetch('http://122.51.6.129:8001/admin/login/index', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password,
         })
-      }, 2000);
+      }).then(res => res.json())
+        .then(data => {
+          if(data.status === 411) {
+            message.error(data.msg)
+          }
+          if (data.status === 200) {
+            message.success('登录成功')
+            setTimeout(() => {
+              this.$router.push({ name: 'OsWindowViewIndex', params: {}});
+              ipc.invoke(ipcApiRoute.restoreWindow, {width: 980, height: 650}).then(r => {
+                //
+              })
+            }, 500);
+          }
+        }).finally(() => {
+        this.loading = false;
+      })
     }
   }
 };
@@ -50,4 +72,3 @@ export default {
   }
 }
 </style>
-  
